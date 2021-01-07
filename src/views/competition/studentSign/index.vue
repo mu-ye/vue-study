dc041cfd1c079f2207c7778e8cc1f15f777cdbac
mubaisama <2720768262@qq.com>
2021-01-07 16:55:22 +0800

    form demo

--- src/views/competition/studentSign/index.vue
+++ src/views/competition/studentSign/index.vue
@@ -1,177 +1,33 @@
 <template>
   <div>
-    <div style="margin-bottom: 16px">
-      <a-row>
-        <a-col span="12">
-          <a-button type="primary" :disabled="!hasSelected" :loading="loading" @click="studentSign">
-            签到
-          </a-button>
-          <span style="margin-left: 8px">
-            <template v-if="hasSelected">
-              {{ `选择 ${selectedRowKeys.length} 人` }}
-            </template>
-          </span>
-        </a-col>
-        <a-col span="12" style="text-align: right">
-          <div>
-            <a-input-search placeholder="输入单位查询" style="width: 200px" @search="onSearch"/>
-          </div>
-        </a-col>
-      </a-row>
-    </div>
-    <a-table
-      rowKey="id"
-      :columns="columns"
-      :data-source="dataTemp"
-      :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-      :pagination="false"
-    >
-      <span slot="img" slot-scope="text, record">
-        <img
-          style="height:50px;"
-          :src="`http://172.18.10.201:8889/res/file/idCard/${record.idCard}.png`"
-        />
-      </span>
-      <span slot="action" slot-scope="text, record">
-        <div v-if="record.signState == '0'" class="error">未签到</div>
-        <div v-else class="success">已签到</div>
-      </span>
-    </a-table>
+    <a-divider> 轮询示例 </a-divider>
+
+    <p> 数值每秒加一 {{ messageText }}</p>
+    <a-button type="primary" @click="onClick"> 暂停 </a-button>
   </div>
 </template>
 <script>
-  const columns = [
-    {
-      title: 'id',
-      dataIndex: 'id',
-      key: 'id'
-    },
-    {
-      title: '编码',
-      dataIndex: 'code',
-      key: 'code'
-    },
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name'
-    },
-    {
-      title: '身份证号',
-      dataIndex: 'idCard',
-      key: 'idCard'
-    },
-    {
-      title: '性别',
-      dataIndex: 'sex',
-      key: 'sex'
-    },
-    {
-      title: '电话',
-      dataIndex: 'phone',
-      key: 'phone'
-    },
-    {
-      title: '所属单位',
-      dataIndex: 'companyName',
-      key: 'companyName'
-    },
-    {
-      title: '照片',
-      key: 'img',
-      scopedSlots: { customRender: 'img' }
-    },
-    {
-      title: '签到状态',
-      key: 'action',
-      scopedSlots: { customRender: 'action' }
-    }
-  ]
-
-  const data = []
-
   export default {
     data () {
       return {
-        data,
-        dataTemp: [],
-        columns,
-        loading: false,
-        selectedRowKeys: [],
-        searchTempList: []
+          messageText: 1
       }
     },
     mounted () {
-      this.init()
-    },
-    computed: {
-      hasSelected () {
-        return this.selectedRowKeys.length > 0
-      }
+      this.stateTimer = setInterval(this.printMessage, 1000)
     },
     methods: {
-      init () {
-        this.axios.get('/student/getStudentList').then(data => {
-          this.data = data
-          this.dataTemp = data
-        })
+      printMessage () {
+        this.messageText += 1
+        console.log('messageText:', this.messageText)
       },
-      studentSign () {
-        this.loading = true
-        // ajax request after empty completing
-        setTimeout(() => {
-          this.loading = false
-          console.log(this.selections)
-
-          this.axios.post('/student/signIn', this.selectedRowKeys).then(() => {
-            this.selectedRowKeys = []
-            this.init()
-          })
-        }, 1000)
-      },
-      onSelectChange (selectedRowKeys, selectedRows) {
-        console.log('selectedRowKeys changed: ', selectedRowKeys)
-        this.selectedRowKeys = selectedRowKeys
-        this.selectedRows = selectedRows
-      },
-      action (text, record) {
-        return record.name + '***' + record.name
-      },
-      onSearch (value) {
-        this.searchTempList = []
-        if (value === '') {
-          this.dataTemp = this.data
-        } else {
-        for (let i = 0; i < this.data.length; i++) {
-          if (this.data[i].companyName.indexOf(value) > -1) { // 此处判断包含，如同like
-            this.searchTempList.push(this.data[i])
-          }
-        }
-        this.dataTemp = this.searchTempList
-        }
+      onClick () {
+        clearInterval(this.stateTimer)
       }
-
-      // studentSign () {
-      //   console.log(this.selectedRowKeys)
-      //  // this.axios.post('', this.rowSelection).then(() => {})
-      // }
+    },
+    destroyed () {
+      clearInterval(this.stateTimer)
     }
+
   }
 </script>
-<style>
-  .success {
-    color: #52c41a;
-  }
-
-  .error {
-    color: #ff4d4f;
-  }
-
-  .link {
-    color: #1890ff;
-  }
-
-  .warning {
-    color: #faad14;
-  }
-</style>
--- src/views/system/company/index.vue
+++ src/views/system/company/index.vue
@@ -1,237 +1,97 @@
 <template>
   <div>
-    <!--    <div class="table-operations">-->
-    <!--      <a-button type="primary" @click="showAddDrawer">-->
-    <!--        新增单位-->
-    <!--      </a-button>-->
-    <!--    </div>-->
-    <a-card>
-      <div class="table-operations">
-        <a-upload
-          name="file"
-          :multiple="false"
-          action="/njdt/company/uploadCompany"
-          :headers="headers"
-          @change="handleChange"
-        >
-          <a-button>
-            <a-icon type="upload" />
-            导入参赛队数据
-          </a-button>
-        </a-upload>
-      </div>
-      <a-table :columns="columns" :data-source="data" rowKey="id" :pagination="false" style="margin-top: 5px">
-        <span slot="action" slot-scope="text, record">
-          <a-button type="primary" @click="showDrawer(record)">
-            编辑
-          </a-button>
-          <!--        <a-divider type="vertical" />-->
-          <!--        <a-button type="danger" @click="showConfirm(record.id)">-->
-          <!--          删除-->
-          <!--        </a-button>-->
-        </span>
-      </a-table>
-      <a-drawer
-        title="编辑信息"
-        :width="480"
-        :visible="visible"
-        :body-style="{ paddingBottom: '80px' }"
-        @close="onClose"
-      >
-        <a-form-model :form="form">
-          <a-form-model-item label="单位编码">
-            <a-input v-model="form.code" />
-          </a-form-model-item>
-          <a-form-model-item label="单位名">
-            <a-input v-model="form.name" />
-          </a-form-model-item>
-          <a-form-model-item label="单位简介">
-            <a-input v-model="form.introduction" />
-          </a-form-model-item>
-          <a-form-model-item label="领队姓名">
-            <a-input v-model="form.leaderName" />
-          </a-form-model-item>
-          <a-form-model-item label="领队电话">
-            <a-input v-model="form.leaderPhone" />
-          </a-form-model-item>
-          <a-form-model-item :wrapper-col="{ span: 12, offset: 5 }">
-            <a-button type="primary" @click="handleSubmit">
-              提交修改
-            </a-button>
-          </a-form-model-item>
-        </a-form-model>
-      </a-drawer>
-      <a-drawer
-        title="新增单位"
-        :width="480"
-        :visible="addVisible"
-        :body-style="{ paddingBottom: '80px' }"
-        @close="onClose"
-      >
-        <a-form-model :form="form">
-          <a-form-model-item label="单位编码">
-            <a-input v-model="form.code" />
-          </a-form-model-item>
-          <a-form-model-item label="单位名">
-            <a-input v-model="form.name" />
-          </a-form-model-item>
-          <a-form-model-item label="单位简介">
-            <a-input v-model="form.introduction" />
-          </a-form-model-item>
-          <a-form-model-item label="领队姓名">
-            <a-input v-model="form.leaderName" />
-          </a-form-model-item>
-          <a-form-model-item label="领队电话">
-            <a-input v-model="form.leaderPhone" />
-          </a-form-model-item>
-          <a-form-model-item :wrapper-col="{ span: 12, offset: 5 }">
-            <a-button type="primary" @click="handleAddSubmit">
-              新增单位
-            </a-button>
-          </a-form-model-item>
-        </a-form-model>
-      </a-drawer>
-    </a-card>
+    <a-divider> form-model 使用示例</a-divider>
+    <a-divider orientation="left"> 知识点</a-divider>
+    <p>
+      1.
+    </p>
+    <a-divider orientation="left"> demo </a-divider>
+    <a-form-model
+      :model="formData"
+      :rules="rules"
+      ref="ruleForm"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol">
+      <a-form-model-item label="用户名" prop="username" >
+        <a-input v-model="formData.username"></a-input>
+      </a-form-model-item>
+      <a-form-model-item label="性别" prop="sex">
+        <a-select v-model="formData.sex" >
+          <a-select-option value="1">
+            男
+          </a-select-option>
+          <a-select-option value="2">
+            女
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item label="选择日期" required prop="date">
+        <a-date-picker
+          v-model="formData.date"
+          type="date"
+          show-time
+          placeholder="Pick a date"
+          format="YYYY-MM-DD HH:mm:ss"
+          style="width: 100%;"
+        />
+      </a-form-model-item>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click="onSubmit">
+          提交
+        </a-button>
+        <a-button style="margin-left: 10px;" @click="resetForm">
+          取消
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
   </div>
 </template>
-
 <script>
-const columns = [
-  {
-    title: '单位名',
-    dataIndex: 'name',
-    key: 'name'
-  },
-  {
-    title: '单位编码',
-    dataIndex: 'code',
-    key: 'code'
-  },
-  {
-    title: '单位简介',
-    dataIndex: 'introduction',
-    key: 'introduction'
-  },
-  {
-    title: '领队名',
-    dataIndex: 'leaderName',
-    key: 'leaderName'
-  },
-  {
-    title: '领队电话',
-    dataIndex: 'leaderPhone',
-    key: 'leaderPhone'
-  },
-  {
-    title: '操作',
-    key: 'action',
-    scopedSlots: { customRender: 'action' }
-  }
-]
+  import request from '@/utils/request'
 
-const data = []
-
-export default {
-  data () {
-    return {
-      data,
-      columns,
-      visible: false,
-      addVisible: false,
-      form: {
-        id: 0,
-        code: '',
-        name: '',
-        introduction: '',
-        leaderName: '',
-        leaderPhone: ''
-      },
-      headers: {
-        authorization: 'authorization-text'
-      }
-    }
-  },
-  mounted () {
-    this.init()
-  },
-  methods: {
-    init () {
-      this.axios.get('/company/getCompanyList').then(data => {
-        console.log(data)
-        this.data = data
-      })
-    },
-    showDrawer (record) {
-      console.log(record)
-      this.visible = true
-      this.form.id = record.id
-      this.form.code = record.code
-      this.form.name = record.name
-      this.form.introduction = record.introduction
-      this.form.leaderName = record.leaderName
-      this.form.leaderPhone = record.leaderPhone
-    },
-    onClose () {
-      this.visible = false
-    },
-    handleSubmit () {
-      this.axios
-        .post('/company/editCompany', this.form)
-        .then(data => {
-          this.$message.info('修改成功')
-          this.visible = false
-        })
-        .catch(err => console.log(err.data))
-    },
-    showAddDrawer () {
-      this.addVisible = true
-      this.form = {
-        id: undefined,
-        code: '',
-        name: '',
-        introduction: '',
-        leaderName: '',
-        leaderPhone: ''
-      }
-    },
-    handleAddSubmit () {
-      this.axios.post('/company/addCompany', this.form).then(() => {
-        this.$message.info('新增成功')
-        this.addVisible = false
-      })
-    },
-    deleteCompany (id) {
-      this.axios.delete('/company/deleteCompany/' + id).then(data => {
-        if (data.success) {
-          this.$message.info('删除成功！')
-        } else {
-          this.$message.error(data.errorMessage)
-        }
-      })
-      console.log(id)
-    },
-    showConfirm (id) {
-      const that = this
-      this.$confirm({
-        title: '确认删除?',
-        // content: 'When clicked the OK button, this dialog will be closed after 1 second',
-        onOk () {
-          that.deleteCompany(id)
+  export default {
+    data () {
+      return {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 },
+        formData: {
+          username: '',
+          sex: undefined,
+          date: undefined
         },
-        onCancel () {}
-      })
+        rules: {
+          username: [{ message: '请输入用户名', required: true, trigger: ['change', 'blur'], whitespace: true }],
+          sex: [{ message: '选择姓名', required: true, trigger: ['change', 'blur'], whitespace: true }],
+          date: [{ message: '选择日期', required: true, trigger: ['change', 'blur'] }]
+        }
+      }
     },
-    handleChange (info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList)
+    methods: {
+      onSubmit () {
+        this.$refs.ruleForm.validate(valid => {
+          console.log(this.formData.date.format('YYYY-MM-DD HH:mm:ss'))
+          if (valid) {
+            request.get('/form', { params: {
+                ...this.formData,
+                date: this.formData.date.format('YYYY-MM-DD HH:mm:ss')
+              }
+
+            }).then(data => {
+              console.log(data)
+            }).catch(e => {
+              console.log(e)
+            })
+          } else {
+            // 验证失败执行的操作
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      resetForm () {
+        this.$refs.ruleForm.resetFields()
       }
-      if (info.file.status === 'done') {
-        this.$message.success(`${info.file.name} 上传成功！`)
-        this.init()
-      } else if (info.file.status === 'error') {
-        this.$message.error(`${info.file.name} 上传失败`)
-      }
+
     }
   }
-}
 </script>
